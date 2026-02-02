680e07dd7bdc615e031b9890c01fe973e3bdcc9e
Trevin Chow <trevin@bigcartel.com>
2026-02-02 15:57:00 -0800

    Update hzl formula to 1.13.2

--- Formula/hzl.rb
+++ Formula/hzl.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Hzl < Formula
   desc "External task ledger for coding agents and OpenClaw"
   homepage "https://github.com/tmchow/hzl"
-  url "https://registry.npmjs.org/hzl-cli/-/hzl-cli-1.9.2.tgz"
-  sha256 "89a486c1f221cc52f115c3fae6178ac3cc8576e53e367bfddb36c1f9ba8e3682"
+  url "https://registry.npmjs.org/hzl-cli/-/hzl-cli-1.13.2.tgz"
+  sha256 "0dac716fd40fb38945706a6e0a6dfd5e1dd3b533fa4c66217648ccae0cee39a0"
   license "MIT"
 
   depends_on "node"
